ed3fbb2a74cddb9c8541c45c246e2f486884dcef
gvilarc <gabriela.vilar.carneiro@gmail.com>
2024-07-08 09:50:10 -0300

    Add files via upload

--- Blockchain.c
+++ Blockchain.c
@@ -1,67 +1,80 @@
-#include <studio.h>
+#include <stdio.h>
 #include <string.h>
-#include <stbool.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #define N 100
 
-typedef struct pessoa { // struct para cada pessoa
+typedef struct { // struct para cada pessoa
 
+    int id; //importante ter um identificador único para cada membro da blockchain já que podem ter nomes iguais
     char nome[N];
-    float conta_b; // conta bancária de cada pessoa (pra calcular o saldo etc)
+    float saldo; 
 
 } pessoa;
 
-typedef struct transacao { // struct para as transações
+typedef struct { // struct para as transações
 
-    int id_transacao; // identificador
+    int id; // identificador
     pessoa remetente;
     pessoa destinatario;
-    float valor_pago;
+    float valor;
 
 } transacao;
 
 typedef struct bloco { // struct para os blocos
 
-    int id_bloco;
-    transacao transacoes[100]; // vetor de transações (bizarro e genial)
+    int id;
+    transacao* transacoes; 
     int num_transacoes;
-    struct bloco *anterior; // chain entre os blocos em sequência
+    struct bloco* proximo; // chain entre os blocos em sequência
 
 } bloco;
 
-typedef struct blockchain { // struct para conexão dos blocos
+typedef struct { // struct para conexão dos blocos
 
-    bloco *head;  // primeiro bloco
-    bloco *atual; // bloco atual
-    int t_max_bloco; // tamanho máximo do bloco
-    int num_blocos;
+    int id; //identificador para sabermos de qual blockchain a transacao informada pertence, já que o programa deve suportar mais de uma
+    bloco* head;  // primeiro bloco
+    bloco* atual; // bloco atual
+    int t_max_bloco; // tamanho máximo do bloco (número de transações por bloco)
+    int num_blocos;  // quantidade de blocos
 
 } blockchain;
 
-blockcahin *definir_blockchain (int t_bloco) {
+blockchain *definir_blockchain (int id, int t_bloco) { //cria a blockchain e tem como parâmetro seu id e o número máximo de transações por bloco
 
     blockchain *bc = (blockchain *)malloc(sizeof(blockchain));
-    if (a == NULL) return -1;
+    if (bc == NULL) return NULL; //nao pode retornar -1 pq retorna blockchain*
 
     // inicialização da estrutura
-    bc->head = NULL;
+    bc->id = id;
+    bc->head = NULL; //blockchain inicialmente vazia
     bc->atual = NULL;
     bc->tam_max_bloco = t_bloco;
     bc->num_blocos = 0;
 
-    return a;
+    return bc;
 
+}
+
+void salvar_blockchain(Blockchain* bc, const char* nome_arquivo) { //criação do arquivo para salvar a blockchain
+    FILE* arquivo = fopen(nome_arquivo, "wb");
+    if (!arquivo) {
+        printf("Erro ao abrir o arquivo para escrita.\n");
+        return;
+    }
+    fwrite(&bc->tam_max_bloco, sizeof(int), 1, arquivo);
 }
 
 bloco *definir_bloco (int id_bloco) {
 
-    bloco b = (bloco *)malloc(sizeof(bloco));
-    if (b == NULL) return -1;
+    bloco* b = (bloco *)malloc(sizeof(bloco)); 
+    if (b == NULL) return NULL;
 
     // inicialização da estrutura
     b->id_bloco = id_bloco;
     b->num_transacoes = 0;
-    b->anterior = NULL;
+    b->transacoes = (transacao*)malloc(sizeof(transacao) * N);
+    b->proximo = NULL;
 
     return b;
 
@@ -88,12 +101,75 @@ void add_transacao (blockchain *bc, pessoa *pagador, pessoa *recebedor, int dinh
     nova_transacao.valor_pago = dinheiro; // gravando o valor transferido
 
     // atualização das contas bancárias (saldo)
-    remetente->conta_b -= dinheiro;
-    destinatario->conta_b += dinheiro;
+    remetente->saldo -= dinheiro;
+    destinatario->saldo += dinheiro;
 
     // atualização da transação ao bloco atual
     bc->atual->transacoes[bc->atual->num_transacoes++] = nova_transacao; // insere no próximo índice do vetor de transações
 
+}
+
+int main()
+{ 
+    /*
+    int id_bc, tam_blobo;
+
+    printf("Criando as blockchains...\n\n");
+    do
+    {
+        printf("Informe o id da blockchain (0 para terminar): ");
+        scanf("%d", &id_bc);
+
+        if (id_bc != 0) 
+        {
+            printf("Informe o número de transações máximo por bloco: ");
+            scanf("%d", &tam_bloco);
+
+            blockchain* bc = definir_blockchain(id_bc, tam_bloco);
+
+            char nomeArq[N] = "bc" + id + ".dat";
+
+            salvar_blockchain(bc, nomeArq);
+        }
+    }
+    while (id_bc != 0);
+
+    int id_transacao, id_remetente, id_destinatario;
+    char nome_remetente[N], nome_destinatario[N];
+    float valor;
+
+    printf("Criando as transações...\n\n");
+    do
+    {
+        printf("Informe o id da transação (0 para terminar): ");
+        scanf("%d", &id_transacao);
+
+        if (id_transacao != 0)
+        {
+            printf("Informe o id da blockchain dessa transação: ");
+            scanf("%d", &id_bc");
+
+            printf("Informe o id do membro remetente: ");
+            scanf("%d", &id_remetente);
+
+            printf("Informe o nome do membro remetente: ");
+            scanf("%s", nome_remetente);
+
+            printf("Informe o id do membro destinatário: ");
+            scanf("%d", &id_destinatario);
+
+            printf("Informe o nome do membro destinatário: ");
+            scanf("%d", nome_destinatario);
+
+            printf("Informe o valor da transação: ");
+            scanf("%d", &valor);
+
+            add_transacao () // e dentro dessa funcao abrir arquivo dessa bc, guardar a transacao   
+        } 
+    }
+    while (id_transacao != 0);
+
+    */
 }
 
 

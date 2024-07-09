32f4aa2f009a795955659c1fb2653bd0cfb4f86d
Ana Luisa Barbosa <blueisas@outlook.com>
2024-07-09 20:48:04 -0300

    Update Blockchain.c

--- Blockchain.c
+++ Blockchain.c
@@ -1,68 +1,27 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include "header.h"
-#define N 100
+#define MAX 50
 
-int main() { 
-    
-    int id_bc, tam_bloco;
+int main() {
+    Blockchain** blockchains = NULL;
+    int num_blockchains = 0;
 
-    printf("Criando as blockchains...\n\n");
-    do {
-        
-        printf("Informe o id da blockchain (0 para terminar): ");
-        scanf("%d", &id_bc);
+    menu(blockchains, &num_blockchains);
 
-        if (id_bc != 0) { //quando o id_bc for zero não irá criar uma blockchain
-        
-            printf("Informe o número de transações máximo por bloco: ");
-            scanf("%d", &tam_bloco);
-
-            blockchain* bc = criar_blockchain(id_bc, tam_bloco); //criamos a blockchain(inicialmente vazia)
-
-            char nome_arquivo[N];
-            snprintf(nome_arquivo, N, "bc%d.dat", id_bc);
-
-            criar_arquivo(bc, nome_arquivo); //criamos o arquivo que vai salvar a blockchain
+    for (int i = 0; i < num_blockchains; i++) {
+        free(blockchains[i]->membros);
+        Bloco* bloco = blockchains[i]->ultimo_bloco;
+        while (bloco != NULL) {
+            Bloco* anterior = bloco->anterior;
+            free(bloco->transacoes);
+            free(bloco);
+            bloco = anterior;
         }
+        free(blockchains[i]);
     }
-    while (id_bc != 0); 
+    free(blockchains);
 
-    int id_transacao, id_remetente, id_destinatario;
-    char nome_remetente[N], nome_destinatario[N];
-    float valor;
-
-    printf("Criando as transações...\n\n");
-    do {
-        
-        printf("Informe o id da transação (0 para terminar): ");
-        scanf("%d", &id_transacao);
-
-        if (id_transacao != 0) { //não irá ser criada uma transação de id 0
-        
-            printf("Informe o id da blockchain dessa transação: ");
-            scanf("%d", &id_bc);
-
-            printf("Informe o id do membro remetente: ");
-            scanf("%d", &id_remetente);
-
-            printf("Informe o nome do membro remetente: ");
-            scanf("%s", nome_remetente);
-
-            printf("Informe o id do membro destinatário: ");
-            scanf("%d", &id_destinatario);
-
-            printf("Informe o nome do membro destinatário: ");
-            scanf("%d", nome_destinatario);
-
-            printf("Informe o valor da transação: ");
-            scanf("%d", &valor);
-
-            add_transacao (id_bc, );   
-        } 
-    }
-    while (id_transacao != 0);
-
+    return 0;
 }

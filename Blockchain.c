ec49f527175e98ff5ac3f36fc3ef3c3881741cb9
Ana Luisa Barbosa <blueisas@outlook.com>
2024-07-09 10:34:10 -0300

    remoção das funções criar

--- Blockchain.c
+++ Blockchain.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include "criacao.h"
 #define N 100
 
 typedef struct { // struct para cada pessoa
@@ -39,51 +40,6 @@ typedef struct { // struct para conexão dos blocos
     int num_blocos;  // quantidade de blocos
 
 } blockchain;
-
-blockchain *criar_blockchain (int id, int t_bloco) { //cria a blockchain e tem como parâmetro seu id e o número máximo de transações por bloco
-
-    blockchain *bc = (blockchain *)malloc(sizeof(blockchain));
-    if (bc == NULL) return NULL; //nao pode retornar -1 pq retorna blockchain*
-
-    // inicialização da estrutura
-    bc->id = id;
-    bc->head = NULL; //blockchain inicialmente vazia
-    bc->atual = NULL;
-    bc->t_max_bloco = t_bloco;
-    bc->num_blocos = 0;
-
-    return bc;
-
-}
-
-void criar_arquivo(Blockchain* bc, const char* nome_arquivo) { //criação do arquivo para salvar a blockchain
-    
-    FILE* arquivo = fopen(nome_arquivo, "wb");
-    if (!arquivo) {
-        
-        printf("Erro ao abrir o arquivo para escrita.\n");
-        return;
-        
-    }
-    
-    fwrite(&bc->t_max_bloco, sizeof(int), 1, arquivo);
-    fclose(arquivo);
-    
-}
-
-bloco *criar_bloco (int id_bloco, int t_bloco) {
-    
-    bloco* b = (bloco *)malloc(sizeof(bloco)); 
-    if (b == NULL) return NULL;
-
-    // inicialização da estrutura
-    b->id = id_bloco;
-    b->num_transacoes = 0;
-    b->transacoes = (transacao*)malloc(sizeof(transacao) * t_bloco);
-    b->proximo = NULL;
-    
-    return b;
-}
 
 void add_transacao (blockchain *bc, pessoa *pagador, pessoa *recebedor, float dinheiro, const char* nome_arquivo) {
 

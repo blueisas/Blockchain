2d667db925443cdc13f5485ce506a3228c90f0b3
Ana Luisa Barbosa <blueisas@outlook.com>
2024-07-09 10:45:22 -0300

    Update and rename criar.h to header.h

--- header.h
+++ header.h
@@ -1,5 +1,43 @@
 #include <stdlib>
 
+typedef struct { // struct para cada pessoa
+
+    int id; //importante ter um identificador único para cada membro da blockchain já que podem ter nomes iguais
+    char nome[N];
+    float saldo; 
+
+} pessoa;
+
+typedef struct { // struct para as transações
+
+    int id; // identificador
+    pessoa remetente;
+    pessoa destinatario;
+    float valor;
+
+} transacao;
+
+typedef struct bloco { // struct para os blocos
+
+    int id;
+    transacao* transacoes; 
+    int num_transacoes;
+    struct bloco* proximo; // chain entre os blocos em sequência
+
+} bloco;
+
+typedef struct { // struct para conexão dos blocos
+
+    int id; //identificador para sabermos de qual blockchain a transacao informada pertence, já que o programa deve suportar mais de uma
+    bloco* head;  // primeiro bloco
+    bloco* atual; // bloco atual
+    int t_max_bloco; // tamanho máximo do bloco (número de transações por bloco)
+    int num_blocos;  // quantidade de blocos
+
+} blockchain;
+
+//---///////////////////////////////////////////////////////////////////////////////////////////////////---
+
 blockchain *criar_blockchain (int id, int t_bloco) { //cria a blockchain e tem como parâmetro seu id e o número máximo de transações por bloco
 
     blockchain *bc = (blockchain *)malloc(sizeof(blockchain));

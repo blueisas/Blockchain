a33eb2f307f8bc4f47a739f606aad6a6cc5f6d52
Ana Luisa Barbosa <blueisas@outlook.com>
2024-07-09 10:54:06 -0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,4 +1,5 @@
 #include <stdlib>
+#define N 100
 
 typedef struct { // struct para cada pessoa
 
@@ -36,7 +37,9 @@ typedef struct { // struct para conexão dos blocos
 
 } blockchain;
 
+
 //---///////////////////////////////////////////////////////////////////////////////////////////////////---
+
 
 blockchain *criar_blockchain (int id, int t_bloco) { //cria a blockchain e tem como parâmetro seu id e o número máximo de transações por bloco
 
@@ -81,4 +84,45 @@ bloco *criar_bloco (int id_bloco, int t_bloco) {
     b->proximo = NULL;
     
     return b;
+}
+
+
+//---///////////////////////////////////////////////////////////////////////////////////////////////////---
+
+
+void add_transacao (blockchain *bc, pessoa *pagador, pessoa *recebedor, float dinheiro, const char* nome_arquivo) {
+
+    // caso a blockchain esteja vazia ou o bloco atual esteja cheio (a ideia é criar um bloco novo, no geral)
+    if (bc->atual == NULL || bc->atual->num_transacoes >= bc->tam_max_bloco) {
+
+        bloco *novo_bloco = criar_bloco(bc->num_blocos++); // passa o id como parâmetro
+        bc->atual = novo_bloco; // o atual bloco na blockchain agora é o novo bloco
+
+        if (bc->head == NULL) bc->head = novo_bloco; // se o primeiro bloco não existir na blockchain, atribuir o novo
+        
+    }
+
+    // atualização da nova transação
+    transacao nova_transacao;
+    nova_transacao.id = bc->atual->num_transacoes; // adicionando o id
+    nova_transacao.remetente = *remetente; // inserindo os nomes dos usuários da transação
+    nova_transacao.destinatario = *destinatario; // ''
+    nova_transacao.valor = dinheiro; // gravando o valor transferido
+
+    // atualização das contas bancárias (saldo)
+    remetente->saldo -= dinheiro;
+    destinatario->saldo += dinheiro;
+
+    // atualização da transação ao bloco atual
+    bc->atual->transacoes[bc->atual->num_transacoes++] = nova_transacao; // insere no próximo índice do vetor de transações
+
+    FILE* arquivo = fopen(nome_arquivo, "ab");
+    if (!arquivo) {
+        printf("Erro ao abrir o arquivo para escrita.\n");
+        return;
+    }
+    
+    fwrite(&nova_transacao, sizeof(transacao), 1, arquivo); //adicionamos a nova transação ao arquivo da blockchain em que ela ocorre
+    fclose(arquivo);
+    
 }
